9cf05dfec391c6bb4f60cf5f50c4d3183cfed117
ritesourceit <sowmyarajesh@outlook.com>
2016-06-08 10:46:32 -0400

    Updated the Homepage

--- Baasha/views.py
+++ Baasha/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader,RequestContext
+
 
 
 def index(request):
-    return render(request,'Baasha/index.html')
-    #return HttpResponse('Baasha/index.html',render)
+	template=loader.get_template("Baasha/index.html")
+	rc=RequestContext(request)
+	return HttpResponse(template.render(rc))
+
+def language(request):
+	template=loader.get_template("Baasha/language.html")
+	rc=RequestContext(request)
+	return HttpResponse(template.render(rc))
